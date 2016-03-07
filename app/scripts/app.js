2b88fe6069806069d2d0b7c89f0921dffd2ab023
mohamed <mo.zaher.salem@gmail.com>
2016-03-07 17:19:48 +0200

    Init Files

---
+++ app/scripts/app.js
@@ -0,0 +1,29 @@
+// init the ScrollMagic slides
+$(function() {
+  var controller,
+    i,
+    slides;
+  controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      triggerHook: 'onLeave'
+    }
+  });
+  slides = document.querySelectorAll('section.panel');
+  i = 0;
+  while (i < slides.length) {
+    new ScrollMagic.Scene({
+        triggerElement: slides[i]
+      })
+      .setPin(slides[i])
+      // .addIndicators()
+      .addTo(controller);
+    i++;
+  }
+});
+
+// easing the animation of transition to next slide
+$(document).ready(function() {
+  $('.btnNextSlide').localScroll({
+    duration: 1000 //time for slider
+  });
+});
